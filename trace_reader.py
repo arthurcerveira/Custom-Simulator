4b59b8703f00033862ded312f9b907871c73e9d0
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2019-08-23 13:29:38 -0300

    Implementa a class DataReader para ler o arquivo trace

--- trace_reader.py
+++ trace_reader.py
@@ -1,3 +1,24 @@
+# Numero de blocos acessados baseado na janela de busca
+blocks = {
+    16: 44,
+    32: 60,
+    64: 76,
+    96: 76,
+    128: 76
+}
+
+size_pu = {
+    0: [1, 1],
+    1: [0.5, 0.5],
+    2: [0.5, 0.5],
+    3: [0.25, 0.25],
+    4: [0.25, 0.75],
+    5: [0.75, 0.25],
+    6: [0.25, 0.75],
+    7: [0.75, 0.25],
+}
+
+
 class VideoData(object):
     def __init__(self):
         self.title = ""
@@ -5,6 +26,7 @@ class VideoData(object):
         self.search_range = 0
         self.candidate_blocks = 0
         self.data_volume = 0
+        self.cu_size = 0
 
     def set_title(self, title):
         self.title = title
@@ -32,3 +54,87 @@ class VideoData(object):
         return string
 
 
+class DataReader(object):
+    def __init__(self, file_name):
+        self.input_file = open(file_name)
+        self.video_data = VideoData()
+
+    def read_data(self):
+        for line in self.input_file:
+            self.process_line(line)
+
+        # TODO
+        # Escrever os dados no arquivo
+
+    def process_line(self, line):
+        # Pula inicio da codificação e do quadro e da CTU
+        if line.startswith('I') or line.startswith('L'):
+            return
+
+        elif line.startswith('U'):
+            self.get_size(line)
+
+        elif line.startswith('P'):
+            self.process_pu(line)
+
+        elif line.startswith('C'):
+            # C <xCand> <yCand>
+            self.video_data.increment_candidate_blocks(1)
+
+        elif line.startswith('F'):
+            # F <xStart> <yStart>
+            self.video_data.increment_candidate_blocks(blocks[self.video_data.search_range])
+
+        elif line.startswith('R'):
+            self.process_retangle(line)
+
+        # Senao se enquadrar em nenhum dos casos, é a linha do título
+        else:
+            self.set_info(line)
+
+    def get_size(self, line):
+        # U <xCU> <yCU> <size>
+        data = line.split()
+        self.video_data.cu_size = int(data[3])
+
+    def process_pu(self, line):
+        # P <sizePU> <idPart> <ref_frame_id>
+        data = line.split()
+        pu = int(data[1])
+        id_part = int(data[2])
+        partition = self.video_data.cu_size * size_pu[pu][id_part]
+        volume = self.video_data.cu_size * partition
+
+        self.video_data.increment_data_volume(volume)
+
+    def process_retangle(self, line):
+        # R < xL > < xR > < yT > < yB >
+        data = line.split()
+
+        x = int(data[2]) - int(data[1])
+        y = int(data[4]) - int(data[3])
+
+        volume = x*y
+
+        self.video_data.increment_data_volume(volume)
+
+        # TODO
+        # Incrementar o numero de blocos
+
+    def set_info(self, line):
+        # <videoName> <width> <height> <searchRange>
+        data = line.split()
+
+        self.video_data.set_title(data[0])
+        self.video_data.set_resolution(data[1], data[2])
+        self.video_data.set_search_range(data[3])
+
+"""
+def main():
+    data_reader = DataReader("mem_trace.txt")
+    data_reader.read_data()
+
+
+if __name__ == "__main__":
+    main()
+"""
