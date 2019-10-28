ba64d5c93234994b2cd0578cf4de54eca4ca2a0e
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2019-10-28 00:21:56 -0300

    Gera dinamicamente um script shell para criar o report do vtune. Cria um log de funcionas invalidas

--- automate_read.py
+++ automate_read.py
@@ -1,4 +1,5 @@
 import os
+import pprint
 import subprocess
 import shutil
 
@@ -26,17 +27,15 @@ VTUNE_REPORT_OUTPUT = "vtune_reader_output.txt"
 
 AUTOMATE_VTUNE_OUTPUT = "automate_vtune_output.txt"
 
-HEADER_VTUNE = "Video encoder;Encoder Configuration;Video sequence;Resolution;Search range;"
+HEADER_VTUNE = "Video encoder;Encoder Configuration;Video sequence;Resolution;Search range;Metric;"
 
+VTUNE_SCRIPT = "vtune_script.sh"
 DIRECTORY_OUTPUT = "result_dir"
 
-ANALYSE_MEM_CMD = ["amplxe-cl", "-collect", "memory-access", "-result-dir", DIRECTORY_OUTPUT, "--"]
-GENERATE_CSV_CMD = ["amplxe-cl", "-report", "top-down", "-result-dir", DIRECTORY_OUTPUT, "-report-output",
-                    VTUNE_REPORT_INPUT, "-format", "csv", "-csv-delimiter", "semicolon"]
-"""
-ANALYSE_MEM_CMD = "amplxe-cl -collect memory-access -result-dir" + DIRECTORY_OUTPUT + "-- "
-GENERATE_CSV_CMD = ""
-"""
+SOURCE_AMPLXE = "source ~/intel/vtune_amplifier_2019/amplxe-vars.sh\n"
+ANALYSE_MEM_CMD = "amplxe-cl -collect memory-access -result-dir " + DIRECTORY_OUTPUT + " -- "
+GENERATE_CSV_CMD = "amplxe-cl -report top-down -result-dir " + DIRECTORY_OUTPUT + " -report-output " \
+                   + VTUNE_REPORT_INPUT + " -format csv -csv-delimiter semicolon\n"
 
 # Encoder Paths
 HM = "../hm-videomem/"
@@ -53,8 +52,8 @@ VIDEO_CFG_PATH = {"HEVC": HM + "cfg/per-sequence/",
 VIDEO_SEQUENCES_PATH = "../video_sequences"
 
 # Parameters
-FRAMES = '9'
-SEARCH_RANGE = ['64', '96', '128', '256']
+FRAMES = '2'
+SEARCH_RANGE = ['64', '96', '128']
 
 
 # Auxiliary Functions
@@ -73,6 +72,10 @@ def generate_cmd_array(command, video_path, video_cfg, cfg, sr):
     cmd_array = [command, '-c', cfg, '-c', video_cfg, '-i', video_path, '-f', FRAMES, '-sr', sr]
 
     return cmd_array
+
+
+def generate_cmd_str(command, video_path, video_cfg, cfg, sr):
+    return f'{command} -c {cfg} -c {video_cfg} -i {video_path} -f {FRAMES} -sr {sr}'
 
 
 def get_video_info(video_path, cfg_path):
@@ -148,6 +151,7 @@ class AutomateVtuneReader:
     def __init__(self):
         self.video_paths = []
         self.data_reader = VtuneReader(VTUNE_REPORT_INPUT)
+        self.invalid_functions = set()
 
         # Cria o arquivo de saida
         with open(AUTOMATE_VTUNE_OUTPUT, 'w+') as output_file:
@@ -155,24 +159,37 @@ class AutomateVtuneReader:
             output_file.write(self.data_reader.modules_header())
 
     @staticmethod
-    def analyse_mem(cmd, video_path, video_cfg, cfg_path, sr):
-        cmd_array = generate_cmd_array(cmd, video_path, video_cfg, cfg_path, sr)
-        vtune_cmd = ANALYSE_MEM_CMD + cmd_array
-        subprocess.run(vtune_cmd)
+    def generate_vtune_script(cmd, video_path, video_cfg, cfg_path, sr):
+        cmd_array = generate_cmd_str(cmd, video_path, video_cfg, cfg_path, sr)
+        vtune_cmd = ANALYSE_MEM_CMD + cmd_array + "\n"
+
+        with open(VTUNE_SCRIPT, "w") as script:
+            script.write("#!/bin/sh\n")
+            script.write(SOURCE_AMPLXE)
+            script.write(vtune_cmd)
+            script.write(GENERATE_CSV_CMD)
 
     @staticmethod
-    def generate_report():
-        subprocess.run(GENERATE_CSV_CMD)
+    def run_vtune_script():
+        subprocess.call(["bash", VTUNE_SCRIPT])
 
-    def process_report(self, title, width, height, encoder, encoder_cfg):
-        self.data_reader.set_info(title, width, height, encoder, encoder_cfg)
+    def process_report(self, title, width, height, encoder, encoder_cfg, sr):
+        self.data_reader.set_info(title, width, height, encoder, encoder_cfg, sr)
         self.data_reader.read_data()
         self.data_reader.save_data()
+
+    def log_invalid_functions(self):
+        self.invalid_functions = self.invalid_functions.union(self.data_reader.function_log)
+        with open("invalid_funtions.py", 'w') as log:
+            log.write("invalid_functions = " + pprint.pformat(self.invalid_functions))
 
     @staticmethod
     def clean():
         os.remove(VTUNE_REPORT_OUTPUT)
         os.remove(VTUNE_REPORT_INPUT)
+        os.remove(VTUNE_SCRIPT)
+        os.remove("str.bin")
+        os.remove("rec.yuv")
 
         # Remove diretorio gerado por vtune
         shutil.rmtree(DIRECTORY_OUTPUT)
@@ -183,11 +200,12 @@ class AutomateVtuneReader:
 
             for cfg, cfg_path in CONFIG[encoder].items():
                 for sr in SEARCH_RANGE:
-                    self.analyse_mem(cmd, video_path, video_info["video_cfg"], cfg_path, sr)
-                    self.generate_report()
+                    self.generate_vtune_script(cmd, video_path, video_info["video_cfg"], cfg_path, sr)
+                    self.run_vtune_script()
 
-                    self.process_report(video_info["title"], video_info["width"], video_info["height"], encoder, cfg)
+                    self.process_report(video_info["title"], video_info["width"], video_info["height"], encoder, cfg, sr)
                     append_output_file(VTUNE_REPORT_OUTPUT, AUTOMATE_VTUNE_OUTPUT)
+                    self.log_invalid_functions()
 
                     self.clean()
 
@@ -212,8 +230,6 @@ def automate_trace():
 
 
 def automate_vtune():
-    # subprocess.Popen("~/intel/vtune_amplifier_2019/amplxe-vars.sh", shell=True)
-
     automate_reader = AutomateVtuneReader()
     automate_reader.video_paths = list_all_videos(VIDEO_SEQUENCES_PATH)
 
