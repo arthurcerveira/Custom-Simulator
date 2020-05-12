3c3642bd2431874de000dd91d2019435a5324b0b
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2020-05-12 15:31:12 -0300

    Adiciona plotCharts para gerar gráficos com Google Script

---
+++ plotCharts.js
@@ -0,0 +1,65 @@
+const hAxisOptions = {
+  slantedText: true,
+  slantedTextAngle: 60
+}
+
+function blockSizesAdopted() {  
+  const sheetName = 'Block-Sizes-Adopted'
+  
+  const sheet = SpreadsheetApp.getActive().getSheetByName(sheetName)
+  const data = sheet.getDataRange().getValues();
+
+  for (var index = 1; index < data.length; index = index + 5) {
+    var rangeString = sheetName + '!C' + (index) + ":AD" + (index + 4);
+    var range = sheet.getRange(rangeString);
+    
+    var video = data[index][0];
+    var cfg = data[index][1];
+    
+    var title = video + ' - ' + cfg
+    
+    var columnChartBuilder = sheet.newChart().asColumnChart();
+    
+    var chart = columnChartBuilder
+      .setTitle(title)
+      .addRange(range)
+      .setPosition(5 + (index * 4), 3, 0, 0)
+      .setTransposeRowsAndColumns(true)
+      .setNumHeaders(1)
+      .setOption('hAxis', hAxisOptions)
+      .build();
+    
+    sheet.insertChart(chart);
+  }
+}
+
+function allBlockSizesAccessed() {
+  const sheetName = 'All-Block-Sizes-Mem-Access'
+  
+  const sheet = SpreadsheetApp.getActive().getSheetByName(sheetName)
+  const data = sheet.getDataRange().getValues();
+
+  for (var index = 1; index < data.length; index = index + 5) {
+    var rangeString = sheetName + '!I' + (index) + ":AN" + (index + 4);
+    var range = sheet.getRange(rangeString);
+    
+    var video = data[index][2];
+    var cfg = data[index][1];
+    
+    var title = video + ' - ' + cfg
+    
+    var columnChartBuilder = sheet.newChart().asColumnChart();
+    
+    var chart = columnChartBuilder
+      .setTitle(title)
+      .addRange(range)
+      .setPosition(5 + (index * 4), 3, 0, 0)
+      .setTransposeRowsAndColumns(true)
+      .setNumHeaders(1)
+      .setOption('hAxis', hAxisOptions)
+      .build();
+    
+    sheet.insertChart(chart);
+  }
+}
+
