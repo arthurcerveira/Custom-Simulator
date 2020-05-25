291b5ceb93aae1e7a2f9e3cd1f0a81ae27675c97
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2020-05-25 14:36:50 -0300

    Implementa função para gerar grafico de acessos à memória absolutos

--- plotCharts.js
+++ plotCharts.js
@@ -1,65 +1,94 @@
 const hAxisOptions = {
   slantedText: true,
-  slantedTextAngle: 60
-}
+  slantedTextAngle: 60,
+};
 
-function blockSizesAdopted() {  
-  const sheetName = 'Block-Sizes-Adopted'
-  
-  const sheet = SpreadsheetApp.getActive().getSheetByName(sheetName)
+function blockSizesAdopted() {
+  const sheetName = "Block-Sizes-Adopted";
+
+  const sheet = SpreadsheetApp.getActive().getSheetByName(sheetName);
   const data = sheet.getDataRange().getValues();
 
   for (var index = 1; index < data.length; index = index + 5) {
-    var rangeString = sheetName + '!C' + (index) + ":AD" + (index + 4);
+    var rangeString = sheetName + "!C" + index + ":AD" + (index + 4);
     var range = sheet.getRange(rangeString);
-    
+
     var video = data[index][0];
     var cfg = data[index][1];
-    
-    var title = video + ' - ' + cfg
-    
+
+    var title = video + " - " + cfg;
+
     var columnChartBuilder = sheet.newChart().asColumnChart();
-    
+
     var chart = columnChartBuilder
       .setTitle(title)
       .addRange(range)
-      .setPosition(5 + (index * 4), 3, 0, 0)
+      .setPosition(5 + index * 4, 3, 0, 0)
       .setTransposeRowsAndColumns(true)
       .setNumHeaders(1)
-      .setOption('hAxis', hAxisOptions)
+      .setOption("hAxis", hAxisOptions)
       .build();
-    
+
+    sheet.insertChart(chart);
+  }
+}
+
+function memoryAccessPerBlockSize() {
+  const sheetName = "Mem-Access-Per-Block-Size";
+
+  const sheet = SpreadsheetApp.getActive().getSheetByName(sheetName);
+  const data = sheet.getDataRange().getValues();
+
+  for (var index = 1; index < data.length; index = index + 5) {
+    var rangeString = sheetName + "!I" + index + ":AN" + (index + 4);
+    var range = sheet.getRange(rangeString);
+
+    var video = data[index][2];
+    var cfg = data[index][1];
+
+    var title = video + " - " + cfg;
+
+    var columnChartBuilder = sheet.newChart().asColumnChart();
+
+    var chart = columnChartBuilder
+      .setTitle(title)
+      .addRange(range)
+      .setPosition(2 + index * 4, 3, 0, 0)
+      .setTransposeRowsAndColumns(true)
+      .setNumHeaders(1)
+      .setOption("hAxis", hAxisOptions)
+      .build();
+
     sheet.insertChart(chart);
   }
 }
 
 function allBlockSizesAccessed() {
-  const sheetName = 'All-Block-Sizes-Mem-Access'
-  
-  const sheet = SpreadsheetApp.getActive().getSheetByName(sheetName)
+  const sheetName = "All-Block-Sizes-Access";
+
+  const sheet = SpreadsheetApp.getActive().getSheetByName(sheetName);
   const data = sheet.getDataRange().getValues();
 
   for (var index = 1; index < data.length; index = index + 5) {
-    var rangeString = sheetName + '!I' + (index) + ":AN" + (index + 4);
+    var rangeString = sheetName + "!I" + index + ":AN" + (index + 4);
     var range = sheet.getRange(rangeString);
-    
+
     var video = data[index][2];
     var cfg = data[index][1];
-    
-    var title = video + ' - ' + cfg
-    
+
+    var title = video + " - " + cfg;
+
     var columnChartBuilder = sheet.newChart().asColumnChart();
-    
+
     var chart = columnChartBuilder
       .setTitle(title)
       .addRange(range)
-      .setPosition(5 + (index * 4), 3, 0, 0)
+      .setPosition(2 + index * 4, 3, 0, 0)
       .setTransposeRowsAndColumns(true)
       .setNumHeaders(1)
-      .setOption('hAxis', hAxisOptions)
+      .setOption("hAxis", hAxisOptions)
       .build();
-    
+
     sheet.insertChart(chart);
   }
 }
-
